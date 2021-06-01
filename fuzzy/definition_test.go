bf91b603f844cf7156367cb30af2e744d7bc6c16
Samuel BIEMONT <sbiemont@qosenergy.com>
2021-06-01 13:18:43 +0200

    move checkIDs into definition

--- fuzzy/definition_test.go
+++ fuzzy/definition_test.go
@@ -16,3 +16,43 @@ func TestNewIDVal(t *testing.T) {
 		So(fsA1.parent, ShouldEqual, &fvA)
 	})
 }
+
+func TestCheckIDs(t *testing.T) {
+	fvA := NewIDValCustom("a", crisp.Set{})
+	fsA1 := NewIDSetCustom("a1", nil, &fvA)
+	fsA2 := NewIDSetCustom("a2", nil, &fvA)
+
+	fvB := NewIDValCustom("", crisp.Set{})
+	fsB1 := NewIDSetCustom("b1", nil, &fvB)
+
+	fvC := NewIDValCustom("c", crisp.Set{})
+	fsC1 := NewIDSetCustom("", nil, &fvC)
+
+	fvABis := NewIDValCustom("a", crisp.Set{})
+	fsABis1 := NewIDSetCustom("a-bis-1", nil, &fvABis)
+
+	fvATer := NewIDValCustom("a-ter", crisp.Set{})
+	fsATer1 := NewIDSetCustom("a1", nil, &fvATer)
+
+	Convey("id val", t, func() {
+		Convey("when ok", func() {
+			So(checkIDs([]IDSet{fsA1, fsA2}), ShouldBeNil)
+		})
+
+		Convey("when same id val uuid", func() {
+			So(checkIDs([]IDSet{fsA1, fsA2, fsABis1}), ShouldBeError, "values: id `a` already defined")
+		})
+
+		Convey("when same id set uuid", func() {
+			So(checkIDs([]IDSet{fsA1, fsA2, fsATer1}), ShouldBeError, "sets: id `a1` already defined")
+		})
+
+		Convey("when no id val uuid", func() {
+			So(checkIDs([]IDSet{fsA1, fsA2, fsB1}), ShouldBeError, "values: id required")
+		})
+
+		Convey("when no id set uuid", func() {
+			So(checkIDs([]IDSet{fsA1, fsA2, fsC1}), ShouldBeError, "sets: id required")
+		})
+	})
+}
